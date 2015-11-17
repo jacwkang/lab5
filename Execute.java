5d9303d0f289f9d34b64c63603e02cca271e4e00
Lincoln Tran <ltran28@calpoly.edu>
2015-11-16 20:52:04 -0800

    added ALU operations for the different instructions

--- Execute.java
+++ Execute.java
@@ -2,4 +2,28 @@
  * Created by homecomputer on 11/16/15.
  */
 public class Execute {
+	public long run(String instruction, int aLUSrcA, int aLUSrcB) {
+		long result = 0;
+		switch(String instruction) {
+			case "lw":
+				result = aLUSrcA = aLUSrcB;
+			case "jal":
+				result = aLUSrcA = aLUSrcB;
+			case "or":
+				result = aLUSrcA | aLUSrcB;
+			case "ori":
+				result = aLUSrcA | aLUSrcB;
+			case "and":
+				result = aLUSrcA & aLUSrcB;
+			case "beq":
+				result = aLUSrcA - aLUSrcA;
+			case "addiu":
+				result = abs(aLUSrcA) + abs(aLUSrcB);
+			case "bne":
+				result = aLUSrcA - aLUSrcA;
+			case "jr":
+				result = aLUSrcA = aLUSrcB;
+		}
+		return result;
+	}
 }
