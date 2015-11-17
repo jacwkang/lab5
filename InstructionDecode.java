b9af01f81862542ddd458d36a1d738cc20b6d4dd
Lincoln Tran <ltran28@calpoly.edu>
2015-11-16 20:23:19 -0800

    started instruction fetch and decode

--- InstructionDecode.java
+++ InstructionDecode.java
@@ -2,4 +2,7 @@
  * Created by homecomputer on 11/16/15.
  */
 public class InstructionDecode {
+	public void run(String instruction){
+		public boolean loadAB = true;
+	}
 }
--- InstructionFetch.java
+++ InstructionFetch.java
@@ -2,4 +2,7 @@
  * Created by homecomputer on 11/16/15.
  */
 public class InstructionFetch {
+	public void run() {
+		programCounter += 4;
+	}
 }
