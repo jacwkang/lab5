95392ba7c91417c7b74ea747bf911115170ca511
Lincoln Tran <ltran28@calpoly.edu>
2015-11-18 10:08:46 -0800

    added some memory and write back stuff

--- MemoryAccess.java
+++ MemoryAccess.java
@@ -2,4 +2,13 @@
  * Created by homecomputer on 11/16/15.
  */
 public class MemoryAccess {
+	public long run(long result, boolean mem_read) {
+		long memory;
+		if(mem_read) {
+			memory = memory;
+		} else {
+			memory = result;
+		}
+		return memory;
+	}
 }
--- WriteBack.java
+++ WriteBack.java
@@ -2,4 +2,9 @@
  * Created by homecomputer on 11/16/15.
  */
 public class WriteBack {
+	public long run(long = aLU_result) {
+		long register_D = 0;
+		register_D = aLU_result;
+		return register_D;
+	}
 }
